0d7b2eaf7fbe42572258e22e28f6002cd3eae185
Ali Maher <ali840085@gmail.com>
2023-04-17 21:17:42 +0200

    first commit

---
+++ lib/screens/deaf/deaf_home.dart
@@ -0,0 +1,27 @@
+import 'package:divergent/screens/deaf/screens/landing_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class DeafHome extends StatefulWidget {
+  const DeafHome({key}) : super(key: key);
+
+  @override
+  _DeafHomeState createState() => _DeafHomeState();
+}
+
+class _DeafHomeState extends State<DeafHome> {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'ASL Detection',
+        theme: ThemeData(
+          primaryColor: Color(0xff375079),
+        ),
+        home: LandingPage());
+  }
+}
